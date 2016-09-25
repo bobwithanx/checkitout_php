bfe79897642993252b3fa8bebc95d6f9b7c853bf
bobwithanx <email@bobmarquis.com>
2016-09-24 19:37:37 -0700

    Updated to jquery 3.1.1 and bootstrap 3.3.7

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,7 +23,7 @@
     @include('layouts.navigation')
 
     <!-- JavaScripts -->
-    <script src="{{ URL::asset('assets/js/jquery.min.js') }}"></script>
+    <script src="{{ URL::asset('assets/js/jquery-3.1.1.min.js') }}"></script>
     <script src="{{ URL::asset('assets/js/bootstrap.min.js') }}"></script>
     <script src="{{ URL::asset('assets/js/jquery.dataTables.min.js') }}"></script>
     <script src="{{ URL::asset('assets/js/dataTables.bootstrap.min.js') }}"></script>
